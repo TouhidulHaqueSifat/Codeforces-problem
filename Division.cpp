0063a50f3f19f19fd8df67c043bbb2a202e9bb60
Touhidul Haque Sifat <touhidul01752@gmail.com>
2025-09-13 20:04:40 +0600

    This is Div. 4 A question

---
+++ Division.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int rating;
+    cin>>rating;
+    if(rating <= 1399){
+        cout<<"Division 4"<<"\n";
+    }
+    else if(rating > 1399 && rating <= 1599){
+        cout<<"Division 3"<<"\n";
+    }
+    else if(rating > 1599 && rating <= 1899){
+        cout<<"Division 2"<<"\n";
+    }
+    else{
+        cout<<"Division 1"<<"\n";
+    }
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
